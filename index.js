9036fb3b1beb48c04805fc0229692f67fa1e5b20
lina abumahfood <lina.abumahfood@gmail.com>
2023-10-02 00:40:32 +0300

    assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+
+let Scuber = 42;
+let blockToFeet = 264;
+function distanceFromHqInBlocks(number) {
+    if ((number - Scuber) > 0) return number - Scuber;
+    else return Scuber - number;
+};
+
+function distanceFromHqInFeet(number) {
+    let feets = distanceFromHqInBlocks(number);
+    if (feets > 0) return distanceFromHqInBlocks(number) * blockToFeet;
+    else return -1 * (distanceFromHqInBlocks(number) * blockToFeet);
+};
+
+function distanceTravelledInFeet(start, destination) {
+    let feetsDistance = (destination - start) * blockToFeet;
+    if (feetsDistance > 0) return feetsDistance;
+    else return (-1 * (feetsDistance));
+};
+function calculatesFarePrice(start, destination) {
+    let feets = distanceTravelledInFeet(start, destination);
+    if (feets <= 400) return 0;
+    else if (400 < feets && feets <= 2000) return ((feets - 400) * 0.02);
+    else if (2000 < feets && feets <= 2500) return 25;
+    else if (feets > 2500) return 'cannot travel that far';
+};
